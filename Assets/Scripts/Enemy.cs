beae0b89181c86e7000d7ee43df555f6a946b1e8
vivo50kk <145774036+vivo50kk@users.noreply.github.com>
2025-11-15 21:26:19 +0800

    添加项目文件。

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int EnemyID;
+    public string EnemyName;
+    public int MaxHealthValue;
+    public int CurrentHealthValue;
+    public int AttackValue;
+    public int DefenseValue;
+    public int BaseMemoryValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
